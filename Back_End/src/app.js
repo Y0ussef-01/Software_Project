458d78a91c1b5fdfe08f07c72d55f843ab76b011
AbdalrahmanEliwa <eliwapro8@gmail.com>
2026-02-27 17:40:02 +0200

    Finshed the task 1 for delevring the next day

--- Back_End/src/app.js
+++ Back_End/src/app.js
@@ -1,17 +1,29 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+
+const Student = require("./models/Student");
+const teacherRoutes = require("./routes/teacher.routes");
+const studentRoutes = require("./routes/student.routes");
+const authRoutes = require("./routes/auth.routes");
+const adminRoutes = require("./routes/admin.routes");
+
 const app = express();
-const bcrypt = require('bcrypt');
-const Student = require('./models/Student');
-const teacherRoutes = require('./routes/teacher.routes');
-const studentRoutes = require('./routes/student.routes');
-const authRoutes = require('./routes/auth.routes');
-const adminRoutes = require('./routes/admin.routes');
-app.use(express.json());
-const cors = require('cors');
-app.use(cors());
-app.use('/teacher', teacherRoutes);
-app.use('/student', studentRoutes);
-app.use('/auth', authRoutes);
-app.use('/admin', adminRoutes);
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  }),
+);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use("/teacher", teacherRoutes);
+app.use("/student", studentRoutes);
+app.use("/auth", authRoutes);
+app.use("/admin", adminRoutes);
 
 module.exports = app;
