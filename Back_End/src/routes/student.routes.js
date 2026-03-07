9b19e6388ba9ec0f01a8dafacc16e1a7ddbb0c54
Youssef Ashraf <10812023100443@stud.cu.edu.eg>
2026-03-08 00:18:51 +0200

    Add getAllCourses method to Student

--- Back_End/src/routes/student.routes.js
+++ Back_End/src/routes/student.routes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const studentAuth = require('../middlewares/studentAuth');
 const controller = require('../Controllers/student.controller');
+const {getAllCourses} = require("../Controllers/course.controller");
 
 router.get('/Profile', studentAuth, controller.getProfile);
 router.put('/updateProfileImg', studentAuth, controller.updateProfileImg);
 router.put('/updatePassword', studentAuth, controller.updatePassword);
 router.post('/register-course', studentAuth, controller.registerCourse);
 router.delete('/drop-course', studentAuth, controller.dropCourse);
-
-router.get('/my-grades', studentAuth, controller.getMyGrades);
+router.get('/getAllCourses',studentAuth,getAllCourses);
+router.get('/grades', studentAuth, controller.getMyGrades);
 module.exports = router;
