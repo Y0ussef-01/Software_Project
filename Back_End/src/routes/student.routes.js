c4f6e0118374f4696e654f62c3b6314239309d06
Mahmod-Mosad <mmsdf2006@gmail.com>
2026-03-13 16:35:08 +0200

    notification

--- Back_End/src/routes/student.routes.js
+++ Back_End/src/routes/student.routes.js
@@ -2,14 +2,28 @@ const express = require('express');
 const router = express.Router();
 const studentAuth = require('../middlewares/studentAuth');
 const controller = require('../Controllers/student.controller');
-const {getAllCourses} = require("../Controllers/course.controller");
+const { getAllCourses } = require("../Controllers/course.controller");
+const Student = require('../models/Student');
 
+router.post('/attend', studentAuth, controller.registerAttendance);
 router.get('/Profile', studentAuth, controller.getProfile);
 router.put('/updateProfileImg', studentAuth, controller.updateProfileImg);
 router.put('/updatePassword', studentAuth, controller.updatePassword);
 router.post('/register-course', studentAuth, controller.registerCourse);
 router.delete('/drop-course', studentAuth, controller.dropCourse);
-router.get('/getAllCourses',studentAuth,getAllCourses);
+router.get('/getAllCourses', studentAuth, getAllCourses);
 router.get('/grades', studentAuth, controller.getMyGrades);
 router.put('/switch-group', studentAuth, controller.switchGroup);
+router.put('/registerToken', studentAuth, async (req, res) => {
+    try {
+        const { pushToken } = req.body;
+        await Student.findByIdAndUpdate(req.user.id, { pushToken });
+        res.json({ message: 'Token saved' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+router.get('/notifications', studentAuth, controller.getNotifications);
+router.put('/notifications/read', studentAuth, controller.markAllRead);
+
 module.exports = router;
