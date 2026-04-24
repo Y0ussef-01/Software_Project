d2068535e2904909971e3edcc48e184cd2724eb7
Youssef Ashraf <10812023100443@stud.cu.edu.eg>
2026-04-24 16:11:56 +0300

    Fix React Routing 404 on Vercel

--- Front_End/src/api/axiosInstance.js
+++ Front_End/src/api/axiosInstance.js
@@ -1,21 +1,25 @@
 import axios from "axios";
 
+const isDevelopment = import.meta.env.MODE === 'development';
+
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "https://software-project-5413.onrender.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: isDevelopment
+        ? "http://localhost:5000"
+        : "https://software-project-5413.onrender.com",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error),
 );
 
 export default axiosInstance;
